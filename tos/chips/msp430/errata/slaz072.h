c7bf4345ce41bee2c1fd47d7059f2631eae4e06d
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2011-02-16 23:00:49 +0900

    Suport msp430g2452 and exp430g2452

---
+++ tos/chips/msp430/errata/slaz072.h
@@ -0,0 +1,76 @@
+/* -*- mode: c; mode: flyspell-prog; -*- */
+/* Copyright (c) 2011, Tadashi G Takaoka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of Tadashi G. Takaoka nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _H_slaz072_h
+#define _H_slaz072_h
+
+/* See SLAZ072 MSP430G2x52, G2x32, G2x12, G2x02 Device Erratasheet, Revised December 2010 */
+
+#if defined(__MSP430_G2102__) || defined(__MSP430_G2112__) || \
+    defined(__MSP430_G2132__) || defined(__MSP430_G2152__) || \
+    defined(__MSP430_G2202__) || defined(__MSP430_G2212__) || \
+    defined(__MSP430_G2232__) || defined(__MSP430_G2252__) || \
+    defined(__MSP430_G2302__) || defined(__MSP430_G2312__) || \
+    defined(__MSP430_G2332__) || defined(__MSP430_G2352__) || \
+    defined(__MSP430_G2402__) || defined(__MSP430_G2412__) || \
+    defined(__MSP430_G2432__) || defined(__MSP430_G2452__)
+#if !defined(__MSP430_REV__)
+#warning "__MSP430_REV__ not defined, default to 'A'"
+#define __MSP430_REV__ 'A'
+#endif
+#if __MSP430_REV__ == 'A'
+#define ERRATA_BCL12
+#define ERRATA_BCL14
+#define ERRATA_CPU4
+#define ERRATA_SYS15
+#define ERRATA_TA12
+#define ERRATA_TA16
+#define ERRATA_TA22
+#define ERRATA_USI4
+#define ERRATA_USI5
+#define ERRATA_XOSC5
+#endif
+
+#else
+#error "This errata/slaz072.h is for MSP430G2x52/G2x32/G2x12/G2x02"
+#endif
+
+#endif
+
+/*
+ * Local Variables:
+ * c-file-style: "bsd"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim: set et ts=4 sw=4:
+ */
