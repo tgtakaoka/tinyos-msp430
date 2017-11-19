0b08cc1d6b55fba9b7e42c7d8db63b16ab8f934a
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2017-11-19 18:18:57 +0900

    Update msp430 silicon errata
    Added MSP430FR2533.

---
+++ tos/chips/msp430-small/errata/slaz661.h
@@ -0,0 +1,73 @@
+/* -*- mode: c; mode: flyspell-prog; -*- */
+/* Copyright (c) 2017, Tadashi G. Takaoka
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of Tadashi G. Takaoka nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _H_slaz661_h
+#define _H_slaz661_h
+
+/* See SLAZ661H MSP430FR2533 Device Erratasheet, Revised October 2017 */
+
+#if defined(__MSP430FR2533__)
+
+#if !defined(__MSP430_REV__)
+#warning "__MSP430_REV__ not defined, default to 'A'"
+#define __MSP430_REV__ 'A'
+#endif
+
+#if __MSP430_REV__ == 'A'
+#define ERRATA_ADC50
+#define ERRATA_ADC63
+#define ERRATA_BSL18
+#define ERRATA_CPU21
+#define ERRATA_CPU22
+#define ERRATA_CPU40
+#define ERRATA_CPU46
+#define ERRATA_EEM23
+#define ERRATA_PORT28
+#define ERRATA_USCI42
+#define ERRATA_USCI45
+#define ERRATA_USCI47
+#endif
+
+#else
+#error "This errata/slaz661.h is for MSP430FR2533"
+#endif
+
+#endif
+
+/*
+ * Local Variables:
+ * c-file-style: "bsd"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim: set et ts=4 sw=4:
+ */
